fdb5b2671df96dbc000384868fde9eb3a76fa318
fchau1 <fchau@nevada.unr.edu>
2023-12-07 23:41:42 -0800

    christian copy + fixes

--- Drift/pages/SplashPage.js
+++ Drift/pages/SplashPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, SafeAreaView, View, Text, TouchableOpacity } from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import SplashStars from '../assets/splashStars.jpg';
+//import SplashStars from '../assets/splashStars.jpg';
 // import Icon from 'react-native-ico-material-design';
 
 const SplashPage = ({navigation}) => {
