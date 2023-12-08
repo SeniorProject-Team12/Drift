71c0bd424ba477c751b26ba9bf9764e899e21a91
christiankjackson <christian.jacksonyt@gmail.com>
2023-12-07 21:48:50 -0800

    Test post page functionality

--- Drift/pages/PostPage.js
+++ Drift/pages/PostPage.js
@@ -1,25 +1,159 @@
-import React from "react";
-import { StyleSheet, Text, View, Pressable, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
-const PostPage = ({navigation}) => {
-    return (
-        <View style={styles.container}>
-            <Text>Post Page</Text>
-            <Button 
-                title="Click Here"
-                onPress={() => alert('Button Clicked!')}
-            />
+const PostItemScreen = () => {
+  const [image, setImage] = useState(null);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+
+  useEffect(() => {
+    getPermissionAsync();
+  }, []);
+
+  const getPermissionAsync = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== 'granted') {
+      alert('Sorry, we need camera roll permissions to make this work!');
+    }
+  };
+
+  const selectImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [1, 1], // Square aspect ratio
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+        setImage(result.assets[0].uri);
+    }
+  };
+
+  const handleSubmit = () => {
+    // Implement your logic for submitting the post (e.g., send data to backend)
+    // You can access image, title, description, and price in this function
+    console.log('Image:', image);
+    console.log('Title:', title);
+    console.log('Description:', description);
+    console.log('Price:', price);
+
+    // Clear the form after submission
+    setImage(null);
+    setTitle('');
+    setDescription('');
+    setPrice('');
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.container}>
+        <TouchableOpacity 
+            activeOpacity={1}
+            onPress={selectImage}
+        >
+          <View style={styles.imageContainer}>
+            {image ? (
+              <Image source={{ uri: image }} style={styles.image} />
+            ) : (
+              <Text>Select Image</Text>
+            )}
+          </View>
+        </TouchableOpacity>
+
+        <TextInput
+          style={[styles.input, styles.titleInput]}
+          placeholder="Title"
+          value={title}
+          onChangeText={(text) => setTitle(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Description"
+          multiline
+          numberOfLines={4}
+          value={description}
+          onChangeText={(text) => setDescription(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Price"
+          keyboardType="numeric"
+          value={price}
+          onChangeText={(text) => setPrice(text)}
+        />
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.submitButton}
+            onPress={handleSubmit}
+          >
+            <Text style={{fontWeight: 'bold', fontSize: 20}}>POST ITEM</Text>
+          </TouchableOpacity>
         </View>
-    );
+      </View>
+    </TouchableWithoutFeedback>
+  );
 };
 
-export default PostPage;
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#8fcbbc'
-    },
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#8fcbbc'
+  },
+  imageContainer: {
+    aspectRatio: 1, // Square aspect ratio
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    backgroundColor: '#eee',
+    marginBottom: 16,
+    width: '50%'
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  input: {
+    height: 40,
+    //borderColor: 'gray',
+    //borderWidth: 1,
+    marginBottom: 16,
+    padding: 8,
+    backgroundColor: 'white'
+  },
+  titleInput: {
+    fontSize: 18,
+  },
+  submitButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+    width: 200,
+    height: 60,
+    alignSelf: 'center',
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginBottom: 36, // Adjust as needed
+  },
 });
+
+export default PostItemScreen;
