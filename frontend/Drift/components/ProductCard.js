a8fa032480626da29854ac93f71fd5db18cba3bb
fchau1 <fchau@nevada.unr.edu>
2024-04-23 02:00:46 -0700

    Reformatted + minor changes

--- frontend/Drift/components/ProductCard.js
+++ frontend/Drift/components/ProductCard.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { Image, Text } from "react-native";
-import {Card } from "react-native-paper";
+import { Card } from "react-native-paper";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 const ProductCard = ({ item, cardWidth, showInfo, navigation }) => {
@@ -8,19 +8,21 @@ const ProductCard = ({ item, cardWidth, showInfo, navigation }) => {
     <TouchableOpacity
       style={{ width: cardWidth }}
       onPress={() => {
-        navigation.navigate("Item Details", {item: item });
+        navigation.navigate("Item Details", { item: item });
       }}
     >
       <Card elevation={0}>
         <Card.Content>
           <Image
             source={{
-              uri: item.photoURL
+              uri: item.photoURL,
             }}
             style={{ width: "100%", height: 100 }}
             resizeMode="cover"
           />
-          {showInfo && <Text>{`${item.brand} - ${item.category} $${item.price}`}</Text>}
+          {showInfo && (
+            <Text>{`${item.brand} - ${item.category} $${item.price}`}</Text>
+          )}
         </Card.Content>
       </Card>
     </TouchableOpacity>
