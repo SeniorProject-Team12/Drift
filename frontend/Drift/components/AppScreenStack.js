87f2f47b234f35592d0e29047cfdf9380aa1840f
fchau1 <fchau@nevada.unr.edu>
2024-04-23 01:56:18 -0700

    UI changes to profile, added profile for other users

--- frontend/Drift/components/AppScreenStack.js
+++ frontend/Drift/components/AppScreenStack.js
@@ -6,82 +6,95 @@ import FolderDetailsPage from "../pages/FolderDetailsPage";
 import CartPage from "../pages/CartPage";
 import { IconButton } from "react-native-paper";
 import MainTabScreen from "./MainTabScreen";
-// import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import SellerPage from "../pages/SellerPage";
-import SettingsPage from '../pages/SettingsPage';
+import SettingsPage from "../pages/SettingsPage";
 import SelectedOrderPage from "../pages/SelectedOrderPage";
 import SelectedChatScreen from "../pages/SelectedChatScreen";
+import Conversation from "../pages/chat/Conversation";
+import UserProfile from "../pages/UserProfile";
+import { useNavigation } from "@react-navigation/native";
+
+const HeaderRightComponent = () => {
+  const navigation = useNavigation();
+  return (
+    <IconButton
+      icon="basket"
+      size={20}
+      onPress={() => navigation.navigate("Cart")}
+    />
+  );
+};
 
 const Stack = createStackNavigator();
-// const Tab = createMaterialBottomTabNavigator();
-
 const AppScreenStack = ({ navigation }) => {
-    return (
-      <PaperProvider>
-        <Stack.Navigator initialRouteName="MainTab">
-          <Stack.Screen
-            name="Main"
-            component={MainTabScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Item Details"
-            component={ItemPage}
-            options={{
-              headerBackTitleVisible: false,
-              headerRight: () => (
-                <IconButton
-                  icon="basket"
-                  size={20}
-                  onPress={() => {
-                    navigation.navigate("Cart");
-                  }}
-                />
-              ),
-            }}
-          />
-          <Stack.Screen
-            name="Folder Details"
-            component={FolderDetailsPage}
-            options={{
-              headerBackTitleVisible: false,
-            }}
-            />
-          <Stack.Screen
-            name="Order Details"
-            component={SelectedOrderPage}
-            options={{
-              headerBackTitleVisible: false,
-              // headerLeft: navigation.navigate("Orders")
-            }}
-          />
-          <Stack.Screen
-            name="Cart"
-            component={CartPage}
-            options={{
-              headerBackTitleVisible: false
-            }}
-          />
-          <Stack.Screen
-            name="SelectedChat"
-            component={SelectedChatScreen}
-            options={({route}) => ({
-              title: route.params.nameOfUser,
-              headerBackTitleVisible: false,
-              headerShown: true
-            })}
-          />
-          <Stack.Screen
-            name="Seller"
-            component={SellerPage}
-          />
-          <Stack.Screen
-            name="Settings"
-            component={SettingsPage}
-          />
-        </Stack.Navigator>
-      </PaperProvider>
-    );
-  };
+  return (
+    <PaperProvider>
+      <Stack.Navigator initialRouteName="MainTab">
+        <Stack.Screen
+          name="Main"
+          component={MainTabScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Item Details"
+          component={ItemPage}
+          options={{
+            headerBackTitleVisible: false,
+            headerRight: () => <HeaderRightComponent />,
+            headerTitle: "",
+            headerTransparent: true,
+          }}
+        />
+        <Stack.Screen
+          name="Folder Details"
+          component={FolderDetailsPage}
+          options={{
+            headerBackTitleVisible: false,
+          }}
+        />
+        <Stack.Screen
+          name="Conversation"
+          component={Conversation}
+          options={{
+            headerBackTitleVisible: false,
+          }}
+        />
+        <Stack.Screen
+          name="User Profile"
+          component={UserProfile}
+          options={{
+            headerBackTitleVisible: false,
+          }}
+        />
+        <Stack.Screen
+          name="Order Details"
+          component={SelectedOrderPage}
+          options={{
+            headerBackTitleVisible: false,
+            // headerLeft: navigation.navigate("Orders")
+          }}
+        />
+        <Stack.Screen
+          name="Cart"
+          component={CartPage}
+          options={{
+            headerBackTitleVisible: false,
+          }}
+        />
+        <Stack.Screen
+          name="SelectedChat"
+          component={SelectedChatScreen}
+          options={({ route }) => ({
+            title: route.params.nameOfUser,
+            headerBackTitleVisible: false,
+            headerShown: true,
+          })}
+        />
+        <Stack.Screen name="Seller" component={SellerPage} />
+        <Stack.Screen name="Settings" component={SettingsPage} />
+      </Stack.Navigator>
+    </PaperProvider>
+  );
+};
 
 export default AppScreenStack;
