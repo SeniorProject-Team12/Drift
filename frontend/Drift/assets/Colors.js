e523cc7e88da343d50763a217015fc89af4290b2
fchau1 <fchau@nevada.unr.edu>
2024-04-23 01:58:06 -0700

    Made item page dynamic based on if item is the user's. Also added UI changes

---
+++ frontend/Drift/assets/Colors.js
@@ -0,0 +1,20 @@
+export const colors = {
+    primary: '#4a90e2',
+    secondary: '#7b4397',
+    background: '#f5f5f5',
+    text: '#333',
+    success: '#28a745',
+    error: '#dc3545',
+    warning: '#ffc107',
+    lightBlue: '#68C7C1',
+    darkBlue: '#028391',
+    brown: '#794A3A',
+    red: '#DD5341',
+    orange: '#F57F5B',
+    yellow: '#FACA79',
+    lightYellow: '#F6DCAC',
+    white: '#FFFFFF',
+    vanilla: '#f6eee5',
+    black: 'black'
+};
+  
